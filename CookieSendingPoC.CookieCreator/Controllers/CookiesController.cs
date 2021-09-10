7de083e8a29c52c8012c2a720c72217475d2308b
Piotr Jachowicz <piotr.j@toptal.com>
2021-09-10 11:40:24 +0200

    first commit

---
+++ CookieSendingPoC.CookieCreator/Controllers/CookiesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CookieSendingPoC.CookieCreator.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CookiesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCookie()
+        {
+            Response.Cookies.Append("test", "testValue");
+            return Redirect("https://localhost:44313/");
+        }
+    }
+}
---
+++ CookieSendingPoC.CookieReceiver/Controllers/PingController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CookieSendingPoC.CookieReceiver.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PingController : ControllerBase
+    {
+        
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(Request.Cookies.Single(x => x.Key == "test").Value);
+        }
+    }
+}
